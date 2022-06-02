c95f29d2a133ffa2b371fbc9ced0c437fcf86e90
Marcos Rigel <marcosrigel99@gmail.com>
2022-06-02 00:46:39 -0300

    atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
 function increment() {
